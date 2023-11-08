1828f7feb5f7a9c76d5ee49fd2d85022ffa1df9c
Simon Muntean <simon.muntean@stud.ubbcluj.ro>
2023-11-09 00:31:36 +0200

    lab 1 ray tracer complete

---
+++ lab_1_rt/Geometry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_rt
+{
+    public abstract class Geometry
+    {
+        public Color Color { get; set; }
+        public Material Material { get; set; }
+
+        public Geometry(Material material, Color color)
+        {
+            Material = material;
+            Color = color;
+        }
+
+        public abstract Intersection GetIntersection(Line line, double minDist, double maxDist);
+
+        public abstract Vector Normal(Vector v);
+    }
+}
